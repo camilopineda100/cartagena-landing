938795d4654d4ec01d006fd841e9f1d9f46979b1
Sebastian Pineda <camilopineda100@gmail.com>
2020-10-23 19:23:22 -0500

    feat: add slides buttons

--- src/Components/Body/Destinations/Destinations.jsx
+++ src/Components/Body/Destinations/Destinations.jsx
@@ -8,30 +8,38 @@ function Destinations(){
       <h2 className="destinations-title">DESTINATIONS</h2>
       
       <p className="destinations-subtitle">Discover and share the most amazing destinations around the world</p>
-
+      
       <div className="slider">
-        <div className="slide">
-          <img 
-            className="slide-image"
-            src={`${process.env.PUBLIC_URL}/images/destinations/destination1.jpg`} 
-            alt="destination"
-          />
+        <div className="slides-wrapper">
+          <div className="slide" id="slide1">
+            <img 
+              className="slide-image"
+              src={`${process.env.PUBLIC_URL}/images/destinations/destination1.jpg`} 
+              alt="destination"
+            />
+          </div>
+
+          <div className="slide" id="slide2">
+            <img 
+              className="slide-image"
+              src={`${process.env.PUBLIC_URL}/images/destinations/destination2.jpg`} 
+              alt="destination"
+            />
+          </div>
+
+          <div className="slide" id="slide3">
+            <img 
+              className="slide-image"
+              src={`${process.env.PUBLIC_URL}/images/destinations/destination3.jpg`} 
+              alt="destination"
+            />
+          </div>
         </div>
 
-        <div className="slide">
-          <img 
-            className="slide-image"
-            src={`${process.env.PUBLIC_URL}/images/destinations/destination2.jpg`} 
-            alt="destination"
-          />
-        </div>
-
-        <div className="slide">
-          <img 
-            className="slide-image"
-            src={`${process.env.PUBLIC_URL}/images/destinations/destination3.jpg`} 
-            alt="destination"
-          />
+        <div className="slides-buttons">
+          <a className="slide-button" href="#slide1"></a>
+          <a className="slide-button" href="#slide2"></a>
+          <a className="slide-button" href="#slide3"></a>
         </div>
       </div>
     </div>
